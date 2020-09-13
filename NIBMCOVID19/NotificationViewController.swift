3f71ce516a6228e285531de52b31f7fa01ff6b19
ramesh201 <rameshsheran2@gmail.com>
2020-09-13 09:07:17 +0530

    Notification create, survey add screens designed

---
+++ NIBMCOVID19/NotificationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotificationViewController.swift
+//  NIBMCOVID19
+//
+//  Created by Ramesh Sheran on 9/13/20.
+//  Copyright © 2020 Ramesh Sheran. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
